b7b89a7cac0b16e162c9c93ba4a3166db64d79fe
devjsilva <joao.silva1611@etec.sp.gov.br>
2019-03-27 21:34:49 -0300

    Adicionamos função aos botões

--- AppEmpresaABC/app/src/main/java/br/com/etecia/appempresaabc/LoginActivity.java
+++ AppEmpresaABC/app/src/main/java/br/com/etecia/appempresaabc/LoginActivity.java
@@ -1,14 +1,17 @@
 package br.com.etecia.appempresaabc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText txtUsuario, txtSenha;
+    Button sairSistema;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,18 @@ public class LoginActivity extends AppCompatActivity {
 
         txtUsuario = (EditText) findViewById(R.id.edtUsuario);
         txtSenha = (EditText) findViewById(R.id.edtSenha);
+
+        sairSistema = (Button)findViewById(R.id.btnSair);
+
+        sairSistema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sairSistema();
+            }
+        });
     }
 
-    public void sairApp(View view) {
+    public void sairSistema() {
         finish();
     }
 
@@ -31,6 +43,10 @@ public class LoginActivity extends AppCompatActivity {
         if (usuario.equals("etecia") && senha.equals("etecia")) {
 
           Toast.makeText(getApplicationContext(),"Bem vindo ao sistema",Toast.LENGTH_SHORT).show();
+          //abrir janela
+          //Intent intent= new Intent(getApplicationContext(),MenuPrincipalActivity.class);
+          Intent intent = new Intent(LoginActivity.this, MenuPrincipalActivity.class);
+          startActivity(intent);
 
         }else{
             Toast.makeText(getApplicationContext(),"Usuário ou senha inválidos",Toast.LENGTH_SHORT).show();
