85512634c6459b446a4a2b7f467345ce95c939fc
devjsilva <joao.silva1611@etec.sp.gov.br>
2019-04-17 22:12:41 -0300

    Paginas dos personagens adicionadas

--- Joguinho/app/src/main/java/br/com/etecia/menujoguinho/Mutin_Activity.java
+++ Joguinho/app/src/main/java/br/com/etecia/menujoguinho/Mutin_Activity.java
@@ -2,18 +2,12 @@ package br.com.etecia.menujoguinho;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Mutin_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void abrirImagem(View view) {
-        //switch(view.getId()){
-        //}
+        setContentView(R.layout.mutin_layout);
     }
 }
