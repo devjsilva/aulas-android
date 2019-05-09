3ce1850de9db92d0df81ba0164e637d04aa2d202
devjsilva <joao.silva1611@etec.sp.gov.br>
2019-05-08 22:08:13 -0300

    adicionada função ao menu

--- MenuMaterialDesign/app/src/main/java/br/com/etecia/menumaterialdesign/MainActivity.java
+++ MenuMaterialDesign/app/src/main/java/br/com/etecia/menumaterialdesign/MainActivity.java
@@ -3,6 +3,10 @@ package br.com.etecia.menumaterialdesign;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +21,36 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Criando Menu");
 
         getSupportActionBar().setIcon(R.drawable.ic_arrow_dp);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_teste, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mCompartilhar:
+                Toast.makeText(getApplicationContext(), "Compartilhar", Toast.LENGTH_SHORT).show();
+
+            case R.id.mMarcar:
+                Toast.makeText(getApplicationContext(), "Marcar", Toast.LENGTH_SHORT).show();
+
+            case R.id.mSobre:
+                Toast.makeText(getApplicationContext(), "Sobre", Toast.LENGTH_SHORT).show();
+
+            case R.id.mPesquisar:
+                Toast.makeText(getApplicationContext(), "Pesquisar", Toast.LENGTH_SHORT).show();
+
+            case R.id.mConfigurações:
+                Toast.makeText(getApplicationContext(), "Configurações", Toast.LENGTH_SHORT).show();
+
+            case R.id.mFavoritos:
+                Toast.makeText(getApplicationContext(), "Favoritos", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
