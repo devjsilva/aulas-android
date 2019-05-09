16f9b9b8871640c339daab0466a82036504a07f4
devjsilva <joao.silva1611@etec.sp.gov.br>
2019-05-08 21:19:53 -0300

    adicionado menu

---
+++ MenuMaterialDesign/app/src/main/java/br/com/etecia/menumaterialdesign/MainActivity.java
@@ -0,0 +1,21 @@
+package br.com.etecia.menumaterialdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.idToolBar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Criando Menu");
+
+        getSupportActionBar().setIcon(R.drawable.ic_arrow_dp);
+    }
+}
